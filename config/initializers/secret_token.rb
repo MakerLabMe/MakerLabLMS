5c517caae9b8714754f7e208d3eaa4dd00f5779d
AndySze <andy.sze.mail@gmail.com>
2013-06-04 13:56:37 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakerLabLMS::Application.config.secret_token = 'fc1c1b9714cbaa2a77355c530285ec628fcddc1055b60546e6d792cb36c513a8c9d37986c418ae6c2e937739ac4094d2bf452a5db037b1040bba77ad5de2bcfa'
