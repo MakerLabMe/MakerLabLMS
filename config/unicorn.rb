d9de8a150ea06d47c6ce682442e31d8ea15905c3
AndySze <andy.sze.mail@gmail.com>
2013-09-04 10:22:24 +0800

    add nginx and unicorn script

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+root = "/home/rabel/MakerLabLMS/"
+#root = "/vagrant"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.makerlab.sock"
+worker_processes 2
+timeout 30
